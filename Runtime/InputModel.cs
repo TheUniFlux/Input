0be3c22e0e7898737c30495c2b97b4a5f54114b3
Xavier Arpa <arpaxavier@gmail.com>
2023-04-10 12:51:08 +0200

    - fixes

--- Runtime/InputModel.cs
+++ Runtime/InputModel.cs
@@ -1,18 +1,32 @@
+/*
+Copyright (c) 2023 Xavier Arpa López Thomas Peter ('Kingdox')
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace UniFlux.Module.Input
+namespace Kingdox.UniFlux.Input
 {
     [Serializable] public struct InputModel
     {
-        private static readonly Dictionary<string, Func<KeyCode, bool>> FUNCTIONS = new Dictionary<string, Func<KeyCode, bool>>()
-        {
-            [nameof(UnityEngine.Input.GetKey)] = (Code) => UnityEngine.Input.GetKey(Code),
-            [nameof(UnityEngine.Input.GetKeyUp)] = (Code) => UnityEngine.Input.GetKeyUp(Code),
-            [nameof(UnityEngine.Input.GetKeyDown)] = (Code) => UnityEngine.Input.GetKeyDown(Code),
-        };
-        //
         [SerializeField] private string _id;
         [SerializeField] private KeyCode _code;
         private Func<KeyCode, bool> _function;
@@ -24,7 +38,7 @@ namespace UniFlux.Module.Input
         {
             this._id = data.id;
             this._code = data.code;
-            this._function = FUNCTIONS[data.method];
+            this._function = InputData.FUNCTIONS[data.method];
             this._onInput = onInput;
         }
         //
